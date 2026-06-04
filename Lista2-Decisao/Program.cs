bb0fdea6937291dd91146e880d43dfb6a1865cd8
giogio658 <giovanneafonso@hotmail.com>
2026-06-04 19:19:50 -0300

    Add files via upload

---
+++ Lista2-Decisao/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Digite o primeiro valor:");
+int v1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo valor:");
+int v2 = int.Parse(Console.ReadLine());
+if (v1 > v2)
+{
+    Console.WriteLine("O maior valor é: " + v1);
+}
+else
+{
+    Console.WriteLine("O maior valor é: " + v2);
+}
